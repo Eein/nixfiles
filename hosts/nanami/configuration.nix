76b58c5682205100d3c415e14c041e5bd03c9fa9
William Volin <me@williamvolin.com>
2024-07-15 09:24:47 -0700

    dawntrail fixes

--- hosts/nanami/configuration.nix
+++ hosts/nanami/configuration.nix
@@ -5,9 +5,29 @@
 { config, ... }:
 
 {
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.nvidia.acceptLicense = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
   networking.hostName = "nanami"; # Define your hostname.
-  # this needs to be fixed
+# this needs to be fixed
   boot.loader.efi.efiSysMountPoint = "/boot";
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  hardware.nvidia = {
+    modesetting.enable = false;
+    powerManagement.enable = true;
+    open = false;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    dynamicBoost.enable = false;
+    nvidiaSettings = true;
+    prime = {
+      amdgpuBusId = "PCI:4:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
 
   services.auto-cpufreq = {
     enable = true;
@@ -28,21 +48,21 @@
     updater.enable = true;
   };
 
-  boot.extraModprobeConfig = ''
-    blacklist nouveau
-    options nouveau modeset=0
-  '';
-    
-  services.udev.extraRules = ''
-    # Remove NVIDIA USB xHCI Host Controller devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA USB Type-C UCSI devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA Audio devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA VGA/3D controller devices
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-  '';
-  boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
-  services.xserver.videoDrivers = [ "amdgpu" ];
+# boot.extraModprobeConfig = ''
+#   blacklist nouveau
+#   options nouveau modeset=0
+# '';
+
+# services.udev.extraRules = ''
+#   # Remove NVIDIA USB xHCI Host Controller devices, if present
+#   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+#   # Remove NVIDIA USB Type-C UCSI devices, if present
+#   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+#   # Remove NVIDIA Audio devices, if present
+#   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+#   # Remove NVIDIA VGA/3D controller devices
+#   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+# '';
+# boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+# services.xserver.videoDrivers = [ "amdgpu" ];
 }
